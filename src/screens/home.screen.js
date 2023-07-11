22c7fde3942254ce29666f8038f46281c82055ea
kaiyes <kaiyes.ansary@gmail.com>
2023-07-11 17:34:49 +0600

    flatlist added

--- src/screens/home.screen.js
+++ src/screens/home.screen.js
@@ -4,22 +4,30 @@ import {
   Text,
   TouchableOpacity,
   StyleSheet,
-  ScrollView
+  ScrollView,
+  FlatList
 } from 'react-native'
 
-import Kanji from '../util/jlpt5.json'
+import Kanji5 from '../util/jlpt5.json'
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <Text>Bundai だろ？</Text>
-        {Kanji.map((item) => (
-          <Text style={styles.kanjiBlock} key={item.kanjiName}>
-            {item.kanjiName}
-          </Text>
-        ))}
-      </ScrollView>
+      <View style={styles.headerHolder}>
+        <Text style={styles.heading}>Bundai だろ？</Text>
+      </View>
+      <FlatList
+        data={Kanji5}
+        numColumns={5}
+        contentContainerStyle={styles.flatList}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.block}>
+            <Text style={styles.kanjiBlock}> {item.kanjiName}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item.kanjiName}
+      />
     </View>
   )
 }
@@ -28,10 +36,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingTop: '15%',
+    paddingHorizontal: '5%'
   },
   kanjiBlock: {
-    fontSize: 25,
-    fontWeight: '500'
+    fontSize: 30,
+    fontWeight: '500',
+    paddingHorizontal: '2%',
+    paddingVertical: '1%'
+  },
+  flatList: {
+    width: '100%',
+    paddingBottom: '5%'
+  },
+  block: {
+    marginVertical: '2%',
+    marginHorizontal: '2%',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'gray',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  headerHolder: {
+    alignItems: 'flexStart',
+    width: '90%'
   }
 })
