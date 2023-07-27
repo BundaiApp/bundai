11f6ea6d76f786a68291a16de5c8a427d548f7c2
kaiyes <kaiyes.ansary@gmail.com>
2023-07-27 23:54:21 +0600

    section grid, super bad

--- src/screens/jlptKanji.screen.js
+++ src/screens/jlptKanji.screen.js
@@ -6,7 +6,8 @@ import {
   StyleSheet,
   ScrollView,
   FlatList,
-  SectionList
+  SectionList,
+  useWindowDimensions
 } from 'react-native'
 import {
   widthPercentageToDP as wp,
@@ -19,50 +20,31 @@ import Kanji4 from '../util/jlpt4.json'
 import Kanji3 from '../util/jlpt3.json'
 
 export default function Home({ navigation: { navigate } }) {
-  const [dim, setdim] = useState(200)
-  const [items, setItems] = useState([
-    { name: 'TURQUOISE', code: '#1abc9c' },
-    { name: 'EMERALD', code: '#2ecc71' },
-    { name: 'PETER RIVER', code: '#3498db' },
-    { name: 'AMETHYST', code: '#9b59b6' },
-    { name: 'WET ASPHALT', code: '#34495e' },
-    { name: 'GREEN SEA', code: '#16a085' },
-    { name: 'NEPHRITIS', code: '#27ae60' },
-    { name: 'BELIZE HOLE', code: '#2980b9' },
-    { name: 'WISTERIA', code: '#8e44ad' },
-    { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
-    { name: 'SUN FLOWER', code: '#f1c40f' },
-    { name: 'CARROT', code: '#e67e22' },
-    { name: 'ALIZARIN', code: '#e74c3c' },
-    { name: 'CLOUDS', code: '#ecf0f1' },
-    { name: 'CONCRETE', code: '#95a5a6' },
-    { name: 'ORANGE', code: '#f39c12' },
-    { name: 'PUMPKIN', code: '#d35400' },
-    { name: 'POMEGRANATE', code: '#c0392b' },
-    { name: 'SILVER', code: '#bdc3c7' },
-    { name: 'ASBESTOS', code: '#7f8c8d' }
-  ])
+  const { height, width } = useWindowDimensions()
 
-  let arr = {
-    JLPT5: Kanji5,
-    JLPT4: Kanji4,
-    JLPT3: Kanji3
-  }
+  let arr2 = [
+    {
+      title: 'JLPT5',
+      data: Kanji5
+    },
+    {
+      title: 'JLPT4',
+      data: Kanji4
+    }
+  ]
 
   return (
-    <FlatGrid
-      itemDimension={100}
-      data={items}
-      style={styles.gridView}
-      staticDimension={500}
-      //fixed
-      // horizontal
-      spacing={10}
+    <SectionGrid
+      itemDimension={130}
+      sections={arr2}
+      maxDimension={width}
       renderItem={({ item }) => (
-        <View style={styles.itemContainer}>
-          <Text style={styles.itemName}>{item.name}</Text>
-          <Text style={styles.itemCode}>{item.code}</Text>
-        </View>
+        <TouchableOpacity style={styles.block}>
+          <Text style={styles.kanjiText}>{item.kanjiName}</Text>
+        </TouchableOpacity>
+      )}
+      renderSectionHeader={({ section }) => (
+        <Text style={styles.heading}>{section.title}</Text>
       )}
     />
   )
@@ -93,32 +75,5 @@ const styles = StyleSheet.create({
   heading: {
     fontWeight: 'bold',
     fontSize: 20
-  },
-  wrapBox: {
-    flexWrap: true,
-    flexDirection: 'row',
-    flex: 1
-  },
-
-  gridView: {
-    marginTop: 10,
-    flex: 1
-  },
-  itemContainer: {
-    backgroundColor: 'green',
-    borderRadius: 5,
-    padding: 10,
-    height: 150,
-    marginHorizontal: wp('5%')
-  },
-  itemName: {
-    fontSize: 16,
-    color: '#fff',
-    fontWeight: '600'
-  },
-  itemCode: {
-    fontWeight: '600',
-    fontSize: 12,
-    color: '#fff'
   }
 })
