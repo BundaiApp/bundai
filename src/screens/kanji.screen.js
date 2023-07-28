46f04881d7c947b6a7aac293a3e3d32ff3e725ee
kaiyes <kaiyes.ansary@gmail.com>
2023-07-28 09:41:14 +0600

    chat gpt gave some code, has issues 😀

--- src/screens/kanji.screen.js
+++ src/screens/kanji.screen.js
@@ -1,138 +1,84 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import {
   View,
   Text,
-  TouchableOpacity,
-  StyleSheet,
-  ScrollView,
   FlatList,
-  SectionList
+  TouchableOpacity,
+  StyleSheet
 } from 'react-native'
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp
-} from 'react-native-responsive-screen'
 
 import Kanji5 from '../util/jlpt5.json'
 import Kanji4 from '../util/jlpt4.json'
-import Kanji3 from '../util/jlpt3.json'
-import Kanji2 from '../util/jlpt2.json'
-import Kanji1 from '../util/jlpt1.json'
 
-export default function Kanji({ navigation: { navigate } }) {
-  let arr = {
-    JLPT5: Kanji5,
-    JLPT4: Kanji4,
-    JLPT3: Kanji3
-  }
+const columns = 5 // Number of columns you want
+
+const SectionHeader = ({ title }) => (
+  <View style={styles.sectionHeader}>
+    <Text style={styles.sectionHeaderText}>{title}</Text>
+  </View>
+)
+
+const KanjiList = ({ navigate, data }) => {
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.block}
+      onPress={() => navigate('KanjiDetail', { paramsData: item })}>
+      <Text style={styles.kanjiText}>{item.kanjiName}</Text>
+    </TouchableOpacity>
+  )
 
   return (
-    <ScrollView>
-      <View style={styles.spacer} />
-      <Text style={styles.heading}>JLPT5</Text>
-      <View style={styles.wrapBox}>
-        {Kanji5.map(item => (
-          <TouchableOpacity
-            style={styles.block}
-            onPress={() => navigate('KanjiDetail', { paramsData: item })}>
-            <Text style={styles.kanjiText}>{item.kanjiName}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={item => item.kanjiName}
+      numColumns={columns}
+    />
+  )
+}
+
+const YourComponent = ({ navigate }) => {
+  return (
+    <View style={styles.container}>
+      <SectionHeader title="JLPT5" />
+      <KanjiList data={Kanji5} navigate={navigate} />
 
       <View style={styles.spacer} />
 
-      <Text style={styles.heading}>JLPT4</Text>
-      <View style={styles.wrapBox}>
-        {Kanji4.map(item => (
-          <TouchableOpacity
-            style={styles.block}
-            onPress={() => navigate('KanjiDetail', { paramsData: item })}>
-            <Text style={styles.kanjiText}>{item.kanjiName}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <View style={styles.spacer} />
-
-      <Text style={styles.heading}>JLPT3</Text>
-      <View style={styles.wrapBox}>
-        {Kanji3.map(item => (
-          <TouchableOpacity
-            style={styles.block}
-            onPress={() => navigate('KanjiDetail', { paramsData: item })}>
-            <Text style={styles.kanjiText}>{item.kanjiName}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <View style={styles.spacer} />
-
-      <Text style={styles.heading}>JLPT2</Text>
-      <View style={styles.wrapBox}>
-        {Kanji2.map(item => (
-          <TouchableOpacity
-            style={styles.block}
-            onPress={() => navigate('KanjiDetail', { paramsData: item })}>
-            <Text style={styles.kanjiText}>{item.kanjiName}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <View style={styles.spacer} />
-
-      <Text style={styles.heading}>JLPT1</Text>
-      <View style={styles.wrapBox}>
-        {Kanji1.map(item => (
-          <TouchableOpacity
-            style={styles.block}
-            onPress={() => navigate('KanjiDetail', { paramsData: item })}>
-            <Text style={styles.kanjiText}>{item.kanjiName}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <View style={styles.spacer} />
-    </ScrollView>
+      <SectionHeader title="JLPT4" />
+      <KanjiList data={Kanji4} navigate={navigate} />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: '5%'
+    paddingHorizontal: 10,
+    paddingVertical: 20
   },
-  kanjiText: {
-    fontSize: 30,
-    fontWeight: '500',
-    paddingHorizontal: '2%',
-    paddingVertical: '1%'
+  sectionHeader: {
+    backgroundColor: '#f2f2f2',
+    padding: 8
+  },
+  sectionHeaderText: {
+    fontWeight: 'bold',
+    fontSize: 18
   },
   block: {
-    width: wp('12%'),
-    height: wp('12%'),
-    marginVertical: '2%',
-    marginHorizontal: '2%',
-    borderWidth: 1,
-    borderRadius: 5,
-    borderColor: 'gray',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  heading: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    marginLeft: wp('4%'),
-    marginVertical: hp('2%')
-  },
-  wrapBox: {
     flex: 1,
-    flexWrap: true,
-    flexDirection: 'row',
-    paddingHorizontal: wp('2%'),
-    backgroundColor: 'lightgray'
+    margin: 5,
+    padding: 10,
+    backgroundColor: '#e6e6e6',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  kanjiText: {
+    fontSize: 20
   },
   spacer: {
-    height: hp('2%')
+    height: 20
   }
 })
+
+export default YourComponent
