40a64c8fea6f4e0c429a4df05341057111b5cc53
kaiyes <kaiyes.ansary@gmail.com>
2023-10-01 20:27:28 +0600

    added windows

---
+++ windows/bundai/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::bundai::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::bundai::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
