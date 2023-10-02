5f113ecf5453f9eb88624576a885c358c73726cc
kaiyes <kaiyes.ansary@gmail.com>
2023-10-02 19:27:45 +0600

    added sqlite

--- src/screens/exam.screen.js
+++ src/screens/exam.screen.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  ScrollView,
-  FlatList
-} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+import SQLite from 'react-native-sqlite-2'
 
 export default function Exam({ navigation: { navigate } }) {
+  useEffect(() => {
+    const db = SQLite.openDatabase('test.db', '1.0', '', 1)
+    db.transaction(function (txn) {
+      txn.executeSql('DROP TABLE IF EXISTS Users', [])
+      txn.executeSql(
+        'CREATE TABLE IF NOT EXISTS Users(user_id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(30))',
+        []
+      )
+      txn.executeSql('INSERT INTO Users (name) VALUES (:name)', ['nora'])
+      txn.executeSql('INSERT INTO Users (name) VALUES (:name)', ['takuya'])
+      txn.executeSql('SELECT * FROM `users`', [], function (tx, res) {
+        for (let i = 0; i < res.rows.length; ++i) {
+          console.log('item:', res.rows.item(i))
+        }
+      })
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Exam</Text>
