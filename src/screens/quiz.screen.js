5ce052509594c085d80e776a61e71df58e750033
kaiyes <kaiyes.ansary@gmail.com>
2023-11-21 21:57:13 +0600

    made srs notification reactive

--- src/screens/quiz.screen.js
+++ src/screens/quiz.screen.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native'
 import SQLite from 'react-native-sqlite-2'
 //util
 import { FONTS } from '../components/fonts'
@@ -11,6 +12,44 @@ export const QuizScreen = ({ navigation: { navigate } }) => {
   const [data, setData] = useState([])
 
   // Initialize the database
+  useFocusEffect(
+    useCallback(() => {
+      const database = SQLite.openDatabase('srs.db', '1.0', '', 1)
+
+      let d = new Date().toISOString().split('T')[0]
+
+      database.transaction((tx) => {
+        // get the data from sqlite
+        tx.executeSql(
+          'SELECT * FROM flashcards WHERE nextReview =?',
+          [d],
+          // 'SELECT * FROM flashcards',
+          function async(tx, res) {
+            let tempData = []
+            for (let i = 0; i < res.rows.length; ++i) {
+              let row = res.rows.item(i)
+              let quizAnswersArray
+              try {
+                quizAnswersArray = JSON.parse(row.quizAnswers || '[]')
+                meaningsArray = JSON.parse(row.meanings || '[]')
+              } catch (e) {
+                // Handle the error or set a default value if JSON parsing fails
+                quizAnswersArray = []
+              }
+              // Construct the full data object including parsed quizAnswers
+              tempData.push({ ...row, quizAnswers: quizAnswersArray, meanings: meaningsArray })
+            }
+            setData(tempData) // Set state once after collecting all data
+          },
+          function (tx, error) {
+            console.error('Query error:', error)
+          }
+        )
+      })
+      // Cleanup if necessary
+      //return () => database.close()
+    }, [])
+  )
   useEffect(() => {
     const database = SQLite.openDatabase('srs.db', '1.0', '', 1)
     database.transaction((tx) => {
@@ -30,43 +69,6 @@ export const QuizScreen = ({ navigation: { navigate } }) => {
   }, [])
 
   // Initialize the database
-  useEffect(() => {
-    const database = SQLite.openDatabase('srs.db', '1.0', '', 1)
-
-    let d = new Date().toISOString().split('T')[0]
-
-    database.transaction((tx) => {
-      // get the data from sqlite
-      tx.executeSql(
-        'SELECT * FROM flashcards WHERE nextReview =?',
-        [d],
-        // 'SELECT * FROM flashcards',
-        function async(tx, res) {
-          let tempData = []
-          for (let i = 0; i < res.rows.length; ++i) {
-            let row = res.rows.item(i)
-            let quizAnswersArray
-            try {
-              quizAnswersArray = JSON.parse(row.quizAnswers || '[]')
-              meaningsArray = JSON.parse(row.meanings || '[]')
-            } catch (e) {
-              // Handle the error or set a default value if JSON parsing fails
-              quizAnswersArray = []
-            }
-            // Construct the full data object including parsed quizAnswers
-            tempData.push({ ...row, quizAnswers: quizAnswersArray, meanings: meaningsArray })
-          }
-          setData(tempData) // Set state once after collecting all data
-        },
-        function (tx, error) {
-          console.error('Query error:', error)
-        }
-      )
-    })
-    // Cleanup if necessary
-    //return () => database.close()
-  }, [])
-
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.topSection}>
@@ -86,6 +88,9 @@ export const QuizScreen = ({ navigation: { navigate } }) => {
             style={styles.box}
             onPress={() => navigate('SRS_Home', { questionsArray: data })}>
             <Text style={{ ...FONTS.bold24 }}>SRS</Text>
+            <View style={styles.notificationCircle}>
+              <Text style={styles.notificationText}>{data.length}</Text>
+            </View>
           </TouchableOpacity>
           <TouchableOpacity style={styles.box} onPress={() => navigate('QuizSettings')}>
             <Text style={{ ...FONTS.bold24 }}>Instant Quiz</Text>
@@ -130,5 +135,21 @@ const styles = StyleSheet.create({
     height: 100,
     borderRadius: 10,
     marginRight: 15
+  },
+  notificationCircle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: 'red',
+    position: 'absolute',
+    top: -10,
+    right: -10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  notificationText: {
+    fontSize: 18,
+    fontWeight: '400',
+    color: 'white'
   }
 })
