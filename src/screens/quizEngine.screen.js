41c571a959bcfd47dea8c0fc0c398ecfc2e97b78
kaiyes <kaiyes.ansary@gmail.com>
2023-11-21 14:58:36 +0600

    cleaned quiz engine

--- src/screens/quizEngine.screen.js
+++ src/screens/quizEngine.screen.js
@@ -7,11 +7,10 @@ import { FONTS } from '../components/fonts'
 
 export const QuizEngine = ({ navigation, route }) => {
   //route params
-  const { questionsArray, isSRS } = route.params
+  const { questionsArray } = route.params
   //state
   const [number, setNumber] = useState(0)
   const [selectedAns, setSelectedAns] = useState(null)
-  const [data, setData] = useState([])
 
   const moveToNextQuestion = (answer) => {
     setSelectedAns(answer)
@@ -24,54 +23,6 @@ export const QuizEngine = ({ navigation, route }) => {
       }
     }, 500) // Adjust the delay as needed
   }
-
-  // Initialize the database
-  useEffect(() => {
-    const database = SQLite.openDatabase('srs.db', '1.0', '', 1)
-    database.transaction((tx) => {
-      tx.executeSql(
-        `CREATE TABLE IF NOT EXISTS flashcards (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        kanjiName TEXT UNIQUE,
-        hiragana TEXT,
-        meanings TEXT,
-        firstSeen DATE,
-        lastSeen DATE,
-        rating INTEGER,
-        nextReview DATE,
-        quizAnswers TEXT
-      )`
-      )
-
-      tx.executeSql(
-        "SELECT *, date('now') AS currentDate FROM flashcards",
-        [],
-        function (tx, res) {
-          let tempData = []
-          for (let i = 0; i < res.rows.length; ++i) {
-            let row = res.rows.item(i)
-            let quizAnswersArray
-            try {
-              quizAnswersArray = JSON.parse(row.quizAnswers)
-            } catch (e) {
-              // Handle the error or set a default value if JSON parsing fails
-              quizAnswersArray = []
-            }
-            // Construct the full data object including parsed quizAnswers
-            tempData.push({ ...row, quizAnswers: quizAnswersArray })
-          }
-          setData(tempData) // Set state once after collecting all data
-        },
-        function (tx, err) {
-          console.error('Error:', err)
-        }
-      )
-    })
-
-    // Cleanup if necessary
-    // return () => database.close();
-  }, [])
-
   return (
     <View style={styles.container}>
       <View style={styles.topSection}>
@@ -79,45 +30,25 @@ export const QuizEngine = ({ navigation, route }) => {
       </View>
 
       <View style={styles.bottomSection}>
-        {isSRS
-          ? data[number].quizAnswers.map((answer, index) => (
-              <TouchableOpacity
-                key={index}
-                style={[
-                  styles.option,
-                  {
-                    backgroundColor: selectedAns
-                      ? selectedAns === answer
-                        ? questionsArray[number].meanings.includes(answer)
-                          ? 'mediumaquamarine'
-                          : 'salmon'
-                        : 'white'
-                      : 'white'
-                  }
-                ]}
-                onPress={() => moveToNextQuestion(answer)}>
-                <Text style={styles.optionText}>{answer}</Text>
-              </TouchableOpacity>
-            ))
-          : questionsArray[number].quizAnswers.map((answer, index) => (
-              <TouchableOpacity
-                key={index}
-                style={[
-                  styles.option,
-                  {
-                    backgroundColor: selectedAns
-                      ? selectedAns === answer
-                        ? questionsArray[number].meanings.includes(answer)
-                          ? 'mediumaquamarine'
-                          : 'salmon'
-                        : 'white'
-                      : 'white'
-                  }
-                ]}
-                onPress={() => moveToNextQuestion(answer)}>
-                <Text style={styles.optionText}>{answer}</Text>
-              </TouchableOpacity>
-            ))}
+        {questionsArray[number].quizAnswers.map((answer, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[
+              styles.option,
+              {
+                backgroundColor: selectedAns
+                  ? selectedAns === answer
+                    ? questionsArray[number].meanings.includes(answer)
+                      ? 'mediumaquamarine'
+                      : 'salmon'
+                    : 'white'
+                  : 'white'
+              }
+            ]}
+            onPress={() => moveToNextQuestion(answer)}>
+            <Text style={styles.optionText}>{answer}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
     </View>
   )
