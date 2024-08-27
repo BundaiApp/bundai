8337a09b29a64a658b96b186627d6d7b25d1fe16
kaiyes <kaiyes.ansary@gmail.com>
2024-08-27 21:23:53 +0600

    added noun pics

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,32 +1,34 @@
 const fs = require('fs')
 const path = require('path')
 
-// Define the directory containing your files
-const directoryPath = './'
+// Read the JSON file
+const inputFilePath = path.join(__dirname, './nouns_only_words.json')
+const outputFilePath = path.join(__dirname, 'itemsObject.json')
 
-fs.readdir(directoryPath, (err, files) => {
+fs.readFile(inputFilePath, 'utf8', (err, data) => {
   if (err) {
-    return console.log('Unable to scan directory: ' + err)
+    console.error('Error reading the file:', err)
+    return
   }
 
-  files.forEach((file) => {
-    // Extract the file extension
-    const ext = path.extname(file)
+  // Parse the JSON data (which is an array)
+  const itemsArray = JSON.parse(data)
 
-    // Remove numbers, underscores, and the file extension, then re-add the extension
-    const newFileName = file.replace(/_\d+/, '').replace('_', '').replace(ext, '') + ext
+  // Convert the array to an object
+  const itemsObject = itemsArray.reduce((obj, item) => {
+    obj[item] = `require("../assets/${item}.png")`
+    return obj
+  }, {})
 
-    // Build the old and new file paths
-    const oldFilePath = path.join(directoryPath, file)
-    const newFilePath = path.join(directoryPath, newFileName)
+  // Convert the object to a JSON string
+  const outputJson = JSON.stringify(itemsObject, null, 2)
 
-    // Rename the file
-    fs.rename(oldFilePath, newFilePath, (err) => {
-      if (err) {
-        console.log('Error renaming file: ' + err)
-      } else {
-        console.log(`Renamed: ${file} -> ${newFileName}`)
-      }
-    })
+  // Save the result to a new JSON file
+  fs.writeFile(outputFilePath, outputJson, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing the file:', err)
+      return
+    }
+    console.log('File successfully written to', outputFilePath)
   })
 })
